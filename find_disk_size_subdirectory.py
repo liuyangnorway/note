e10376e6c3c2f09ae9e31a9de33463dbec57ac25
liuyangnorway <37050584+liuyangnorway@users.noreply.github.com>
2018-12-24 08:27:55 +0100

    Update find_disk_size_subdirectory.py

--- find_disk_size_subdirectory.py
+++ find_disk_size_subdirectory.py
@@ -80,74 +80,14 @@ def find_size_subdir(directory='.'):
 
 directory='C:\\'
 df=find_size_subdir(directory)
-df.drop(len(df)-1,inplace=True)
-directory='C:\\Windows'
-df2=find_size_subdir(directory)
-df=df.append(df2.iloc[-1])
-total_size = df['total_size'].sum()
-size = size_in_MB_GB(total_size)
-df=df.append([{'path':directory,'size':size,'total_size':total_size}])
-df.reset_index(inplace=True)
-df.drop('index',axis=1,inplace=True)
 
 savefile='{}_subfolder_size_{}.xlsx'.format('_'.join(directory.split(os.sep)).replace(':',''),time.strftime('%Y%m%d',time.localtime(time.time())))
 df.to_excel(r'C:\tmp\{}'.format(savefile))
 
-directory=r'C:\Users\Jingdong\AppData\Roaming'
-df3=find_size_subdir(directory)
-
-directory=r'C:\Users\Jingdong\AppData\Roaming\Apple Computer\MobileSync\Backup' ##very big files
-
-
-directory=r'C:\Users\Jingdong'
+directory=r'C:\Users\用户名\AppData\Roaming'
 df3=find_size_subdir(directory)
 
 
 
-
-def test():           
-    if not os.path.isdir(r'E:\test'):          
-        os.mkdir(r'E:\test')
-    
-        os.mkdir(r'E:\test\A1')
-        os.mkdir(r'E:\test\A2')
-        os.mkdir(r'E:\test\A3')
-        os.mkdir(r'E:\test\A4')
-        os.mkdir(r'E:\test\A1\B11')
-        os.mkdir(r'E:\test\A1\B12')
-        os.mkdir(r'E:\test\A2\B21')
-        os.mkdir(r'E:\test\A3\B31')
-        os.mkdir(r'E:\test\A1\B11\C111')
-    
-    
-    path=r'E:\test'
-    os.chdir(r'E:\test')     
-    ##length =len(path.split(os.sep))+1
-    for root, dirs, files in os.walk(path, topdown=False):  
-        if root ==path:
-        ##if  len(root.split(os.sep))==length:
-            print(dirs)
-    for root, dirs, files in os.walk(".", topdown=False):
-        for name in files:
-            print(os.path.join(root, name))
-        for name in dirs:
-            print(os.path.join(root, name))  
-
-            
-    for root, dirs, files in os.walk(path, topdown=False):
-        print('root={}'.format(root))
-        print('file:')
-        for name in files:        
-            print(os.path.join(root, name))
-        
-        print('Dirs: ')
-        
-        for name in dirs:
-            print(os.path.join(root, name))  
-        print('________________________ ')
-    
-
-
- 
     
 
