d8889c79474f6ed15c6c50c2fe4627aeb124e44f
liuyangnorway <37050584+liuyangnorway@users.noreply.github.com>
2019-01-02 09:27:46 +0100

    Add files via upload

---
+++ Converter_MP4_to_MP3.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  1 20:55:50 2019
+clip = mp.VideoFileClip(filemp4).subclip(0,20) # converter first 20 seconds
+clip = mp.VideoFileClip(filemp4) # converter all mp4 file to mp3 file
+clip.audio.write_audiofile(filemp3)           
+@author: Jingdong
+"""
+
+
+import moviepy.editor as mp
+import os
+
+os.chdir('E:\English\Family_Album_USA')
+
+mp4_file_list=[]
+for root, dirs, files in os.walk('E:\English\Family_Album_USA'):
+   for f in files:
+       filename = os.path.join(root, f)
+       if filename.endswith('.mp4'):
+           mp4_file_list.append(filename)
+
+#clip = mp.VideoFileClip(filemp4).subclip(0,20) # converter first 20 seconds
+
+#clip.audio.write_audiofile(filemp3)           
+           
+for filemp4 in  mp4_file_list:
+    filemp3=filemp4.split('.')[0]+'.mp3' 
+    with mp.VideoFileClip(filemp4) as clip:    
+        clip.audio.write_audiofile(filemp3)
+
+directory=r'E:\English\Oxford English'
+os.chdir(directory)
+mp4_file_list=[]
+for root, dirs, files in os.walk(directory):
+   for f in files:
+       filename = os.path.join(root, f)
+       if filename.endswith('.mp4'):
+           mp4_file_list.append(filename)
+
+#clip = mp.VideoFileClip(filemp4).subclip(0,20) # converter first 20 seconds
+
+#clip.audio.write_audiofile(filemp3)           
+           
+for filemp4 in  mp4_file_list:
+    filemp3=filemp4.split('.')[0]+'.mp3' 
+    with mp.VideoFileClip(filemp4) as clip:    
+        clip.audio.write_audiofile(filemp3)
