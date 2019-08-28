86982982e2477ff9aa093dc65405695fe829035f
Jingdong Liu <37050584+liuyangnorway@users.noreply.github.com>
2019-08-28 22:07:32 +0200

    Update Converter_MP4_to_MP3.py

--- Converter_MP4_to_MP3.py
+++ Converter_MP4_to_MP3.py
@@ -26,6 +26,32 @@ for root, dirs, files in os.walk(directory):
 #clip.audio.write_audiofile(filemp3)           
            
 for filemp4 in  mp4_file_list:
-    filemp3=filemp4.split('.')[0]+'.mp3' 
-    with mp.VideoFileClip(filemp4) as clip:    
-        clip.audio.write_audiofile(filemp3)
+    #filemp3=filemp4.split('.')[0]+'.mp3' 
+    filemp3=filemp4.replace('.mp4','.mp3')
+    if not os.path.isfile(mp3_filename):
+         with mp.VideoFileClip(filemp4) as clip:    
+            clip.audio.write_audiofile(filemp3)
+
+
+ ###########################################################################################        
+
+dataPath=r'E:\**'
+
+MP4_file_list=[]
+for root, dirs, files in os.walk(dataPath):
+    for fname in files:            #if '5001' in fname and fname.endswith('.xlsx') and not fname.startswith('~'):
+        if fname.endswith('.mp4') and not fname.startswith('~'):
+            #print(root,dirs,fname)
+            filename=os.path.join(root, fname)                             
+            MP4_file_list.append(filename) 
+for i in range(len(MP4_file_list)):
+    mp4_filename=MP4_file_list[i]
+    mp3_filename=mp4_filename.replace('.mp4','.mp3')
+    if not os.path.isfile(mp3_filename):
+        try:
+            clip = mp.VideoFileClip(mp4_filename)
+            clip.audio.write_audiofile(mp3_filename)
+            clip.close()
+        except:
+            print ('Could convert {}'.format(mp4_filename))
+        
